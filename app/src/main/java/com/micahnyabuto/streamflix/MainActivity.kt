77c1a883e46a9af691952dbc057ecfc97d73f2c1
Micah Nyabuto <atembamicah@gmail.com>
2025-05-30 23:55:47 +0300

    Update

--- app/src/main/java/com/micahnyabuto/streamflix/MainActivity.kt
+++ app/src/main/java/com/micahnyabuto/streamflix/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.micahnyabuto.streamflix.ui.screens.splash.SplashScreen
 import com.micahnyabuto.streamflix.ui.theme.StreamFlixTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +20,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             StreamFlixTheme {
-
+                SplashScreen()
             }
         }
     }
