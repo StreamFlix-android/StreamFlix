cacbada86c0b53dde07bb761043547cb6d7c7727
Micah Nyabuto <atembamicah@gmail.com>
2025-05-30 23:56:32 +0300

    Added Hilt dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,10 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+
+    id("com.google.devtools.ksp" )
+    id("com.google.dagger.hilt.android")
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.20"
 }
 
 android {
@@ -56,4 +60,42 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //Navigation
+    implementation("androidx.navigation:navigation-compose:2.9.0")
+
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
+
+
+    //Appcompat
+    implementation("androidx.appcompat:appcompat:1.7.0")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+
+    //Extended Material Icons
+    implementation("androidx.compose.material:material-icons-extended:1.7.8")
+
+    //Datastore
+    implementation ("androidx.datastore:datastore-preferences:1.1.6")
+
+    //Retrofit
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+
+    implementation ("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+
+    // JSON serialization library, works with the Kotlin serialization plugin
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+
+
+    //Hilt
+    ksp(libs.hilt.compiler)
+    ksp(libs.androidx.hilt.compiler.v100)
+    implementation(libs.hilt.android.v248)
+    implementation(libs.androidx.hilt.navigation.compose.v110alpha01)
+
+
 }
